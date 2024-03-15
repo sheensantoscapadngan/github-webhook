fe95dad56389eaaec0efc226e619d0d26bb3504e
Sheen Capadngan <sheen@symph.co>
2024-03-15 22:47:27 +0800

    made publishing connector configurable

--- webhook-server/publishers/publisher.go
+++ webhook-server/publishers/publisher.go
@@ -4,10 +4,12 @@ import (
 	"github-webhook/app"
 	branchtagcreationevt "github-webhook/events/branch_tag_creation"
 	pushrepositoryevt "github-webhook/events/push_repository"
+	airopsconnect "github-webhook/publishers/connectors/airops"
 	brainconnect "github-webhook/publishers/connectors/brain"
 	eventspublisher "github-webhook/publishers/events"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 )
 
@@ -51,11 +53,20 @@ func HandlePublishEvents(a *app.App, w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(forPublishSlices) > 0 {
-		// if err := airopsconnect.Publish(forPublishSlices, a.Pool, r.Context()); err != nil {
-		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-		// }
-		if err := brainconnect.Publish(forPublishSlices, a.Pool, r.Context()); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		connector := os.Getenv("CONNECTOR")
+		log.Println("Using publishing connector:", connector)
+
+		switch connector {
+		case "airops":
+			if err := airopsconnect.Publish(forPublishSlices, a.Pool, r.Context()); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		case "brain":
+			if err := brainconnect.Publish(forPublishSlices, a.Pool, r.Context()); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		default:
+			http.Error(w, "no configured connector", http.StatusInternalServerError)
 		}
 	}
 }
